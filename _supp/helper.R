e6206ebf83db3d3ab1608bc8dafe363c3d5ff994
anoiana <anh.stats@gmail.com>
2021-05-09 20:04:45 -0300

    .

--- _supp/helper.R
+++ _supp/helper.R
@@ -6,33 +6,20 @@ if(!require(pacman)) install.packages("pacman");pacman::p_load(
 ##################################
 # functions ----------------------
 # label ----
-lb = function(...,vn=T){
+lb = function(...){
  def1 =paste0(ensyms(...))
  if(length(def1)==1) def1 = c(stringr::str_extract(def1,"[a-z]+"),def1)
 
- if(vn){
-  a = dplyr::case_when(def1[1] == "def"~ "định nghĩa",
-                       def1[1] == "thm" ~ "định lí",
-                       def1[1] == "exm" ~ " ví dụ",
-                       def1[1] == "exr" ~ " bài tập",
-                       def1[1] == "lem" ~ "bổ đề",
-                       def1[1] == "eq" ~ "phương trình",
-                       def1[1] == "fig" ~ "hình"
-  )%>%
-   paste0("*",.,"*")
- }
- else{
-  a = dplyr::case_when(def1[1] == "def"~ "definition",
-                       def1[1] == "thm" ~ "theorem",
-                       def1[1] == "exm" ~ " example",
-                       def1[1] == "exr" ~ " exercise",
-                       def1[1] == "lem" ~ "lemma",
-                       def1[1] == "eq" ~ "equation",
-                       def1[1] == "fig" ~ "figure"
-  )%>%
-   paste0("*",.,"*")
- }
- paste0(colorize(a, color = "black")," \\@ref(",def1[1],":",def1[2],")")%>%
+  a = dplyr::case_when(def1[1] == "def"~ "Def.",
+                       def1[1] == "thm" ~ "Theorem",
+                       def1[1] == "exm" ~ "Ex.",
+                       def1[1] == "exr" ~ "Exercise",
+                       def1[1] == "lem" ~ "Lemma",
+                       def1[1] == "eq" ~ "Eq.",
+                       def1[1] == "fig" ~ "Fig."
+  )%>% paste0("*",.,"*")
+
+ paste0(colorize(a, color = "#424242")," \\@ref(",def1[1],":",def1[2],")")%>%
   str_remove_all('`')
 }
 # function ----
