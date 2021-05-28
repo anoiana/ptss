3266b056b65824abda3edc75a0f8f61b0bc92a81
anoiana <anh.stats@gmail.com>
2021-05-28 02:02:05 -0300

    .

--- _supp/helper.R
+++ _supp/helper.R
@@ -67,7 +67,7 @@ my_text = function(x, font = "Arial", size = 19){
 }
 
 ################## square sign for complete of proof ####################
-proved = function(x = "$\\tiny\\blacksquare$", size = 19){
+proved = function(x = "$\\blacksquare$", size = 18){
   sprintf("<p style='float:right; font-size:%spx'>%s</p></p>", size,x)
 }
 
